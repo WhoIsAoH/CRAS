00ad168ec0dd3722fdd62ebf15dbd08ea445978c
WhoIsAoH <ajay.paudel1@gmail.com>
2024-02-22 13:04:25 +0545

    maintaining some rules

--- ChangeRequestApproval/src/main/java/com/esewa/restchangerequestapproval/RestChangeRequestApprovalApplication.java
+++ ChangeRequestApproval/src/main/java/com/esewa/restchangerequestapproval/RestChangeRequestApprovalApplication.java
@@ -1,4 +1,4 @@
-package com.example.restchangerequestapproval;
+package com.esewa.restchangerequestapproval;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- ChangeRequestApproval/src/test/java/com/esewa/restchangerequestapproval/RestChangeRequestApprovalApplicationTests.java
+++ ChangeRequestApproval/src/test/java/com/esewa/restchangerequestapproval/RestChangeRequestApprovalApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.restchangerequestapproval;
+package com.esewa.restchangerequestapproval;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
