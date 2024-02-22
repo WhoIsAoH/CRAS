ed5cf78e4d1bf910ed2f36667095de016dd6721a
WhoIsAoH <ajay.paudel1@gmail.com>
2024-02-22 12:23:55 +0545

    intialCode

---
+++ ChangeRequestApproval/src/main/java/com/example/restchangerequestapproval/admin/entity/User.java
@@ -0,0 +1,13 @@
+package com.example.restchangerequestapproval.admin.entity;
+
+public class User {
+    private long userId;
+    private String userName;
+    private String userPassword;
+    private String userLocation;
+    private String userRole;
+    private String userEmail;
+    private String userContact;
+    private String userDepartment;
+
+}
---
+++ ChangeRequestApproval/src/test/java/com/example/restchangerequestapproval/RestChangeRequestApprovalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restchangerequestapproval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestChangeRequestApprovalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
