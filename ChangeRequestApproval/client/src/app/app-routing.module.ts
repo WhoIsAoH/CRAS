eb9c6bb8b40c9547d15be1ab19f3362faffa9cf2
Durga Acharya <adurga804@gmail.com>
2024-02-27 13:52:17 +0545

    client side with static forms

--- ChangeRequestApproval/client/src/app/app-routing.module.ts
+++ ChangeRequestApproval/client/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./auth/login/login.component";
+import {HomeComponent} from "./home/home.component";
+import {ChangeRequestFormComponent} from "./form/change-request-form/change-request-form.component";
+import {AddUserFormComponent} from "./admin/add-user-form/add-user-form.component";
+import {UserServiceComponent} from "./services/user-service/user-service.component";
+import {FormServiceComponent} from "./services/form-service/form-service.component";
+import {AdminServiceComponent} from "./services/admin-service/admin-service.component";
+import {RequestedFormComponent} from "./user/requested-form/requested-form.component";
+import {NavbarComponent} from "./header/navbar/navbar.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'form', component: ChangeRequestFormComponent },
+  { path:'', component:LoginComponent},
+  { path:'home', component:HomeComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
+export const RoutingComponent=[
+  LoginComponent,
+  HomeComponent,
+  ChangeRequestFormComponent,
+  AddUserFormComponent,
+  UserServiceComponent,
+  FormServiceComponent,
+  AdminServiceComponent,
+  RequestedFormComponent,
+  NavbarComponent
+]
--- ChangeRequestApproval/client/src/app/app.module.ts
+++ ChangeRequestApproval/client/src/app/app.module.ts
@@ -1,16 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import {AppRoutingModule, RoutingComponent} from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatInputModule} from "@angular/material/input";
+import {MatSelectModule} from "@angular/material/select";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIconModule} from "@angular/material/icon";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from '@angular/material/button';
+import {FormsModule} from "@angular/forms";
+import { AddDepartmentComponent } from './department/add-department/add-department.component';
+import { UpdateDepartmentComponent } from './department/update-department/update-department.component';
+import { DepartmentComponent } from './department/department/department.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RoutingComponent,
+    AddDepartmentComponent,
+    UpdateDepartmentComponent,
+    DepartmentComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatSnackBarModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    MatSelectModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
