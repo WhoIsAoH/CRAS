a6ab061a4195a16ea0b57ed66431ea9120c4639b
Durga Acharya <adurga804@gmail.com>
2024-02-27 13:55:07 +0545

    client side with static forms

--- ChangeRequestApproval/src/main/java/com/esewa/restchangerequestapproval/department/repository/DepartmentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.esewa.restchangerequestapproval.department.repository;
-
-import com.esewa.restchangerequestapproval.department.entity.Department;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartmentRepository extends JpaRepository<Department, Integer> {
-}
